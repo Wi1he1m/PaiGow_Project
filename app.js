e171dc80fd7182e4893a7e989811bf62a394d69b
William L Quinn <sirwill20@hotmail.com>
2018-03-13 02:57:48 +0000

    add server to repo

---
+++ app.js
@@ -0,0 +1,19 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser");
+    
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server started");
+})
